9c965a18efddc9d4b530f4bb9769296459a39e31
Benjamin <bbogush72@gmail.com>
2019-07-30 20:06:26 -0500

    .

--- src/test/java/tests/FirstTest.java
+++ src/test/java/tests/FirstTest.java
@@ -4,5 +4,7 @@ public class FirstTest {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    //git commit -m "first commit"
+    //git push -u origin master
 
 }
